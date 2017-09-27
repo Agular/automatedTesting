f6d44324d715b7a9c749c127c94026c2703aa100
Ragnar <ragnar.luga@gmail.com>
2017-09-27 13:11:31 +0300

    Added test headers.

---
+++ src/tests/ApiTests.java
@@ -0,0 +1,41 @@
+package tests;
+
+import org.junit.Test;
+
+public class ApiTests {
+
+    @Test
+    public void isInputReceiveSuccessful(){
+
+    }
+
+    @Test
+    public void isCurrentTempNumber() {
+
+    }
+
+    @Test
+    public void isGeoLocationCorrect() {
+
+    }
+
+    @Test
+    public void isInputJSON() {
+
+    }
+
+    @Test
+    public void is3DayForecastArrayLengthCorrect() {
+
+    }
+
+    @Test
+    public void isGeoLocationXNumber(){
+
+    }
+
+    @Test
+    public void isGeoLocationYNumber(){
+
+    }
+}
---
+++ src/tests/LogicTests.java
@@ -0,0 +1,26 @@
+package tests;
+
+import org.junit.Test;
+
+public class LogicTests {
+
+    @Test
+    public void is3DayForecastMaxTemperatureCorrect() {
+
+    }
+
+    @Test
+    public void is3DayForecastMinTemperatureCorrect(){
+
+    }
+
+    @Test
+    public void is1DayForecastMaxTemperatureCorrect(){
+
+    }
+
+    @Test
+    public void is1DayForecastMinTemperatureCorrect(){
+
+    }
+}
